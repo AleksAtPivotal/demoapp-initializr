60bb25b20c733d18ff95aba003569e82283776c2
Aleks Saul <asaul@pivotal.io>
2018-10-30 23:54:31 -0400

    webhook based pipeline

---
+++ tools/webhook/concourse_sp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export reponame=$1
+echo "$BIGSECRET" > credentials.yml
+echo PAS_APP_SUFFIX: $reponame >> credentials.yml
+echo PAS_SPACE: $reponame >> credentials.yml
+echo REPONAME: $reponame  >> credentials.yml
+echo GITHUBORGNAME: $reponame >> credentials.yml
+cat credentials.yml
+
+alias fly="/app/fly"
+
+CONCOURSE_USERNAME=$(cat credentials.yml | grep CONCOURSE_USERNAME | awk 'BEGIN { FS = ":" }; { print $2 }' | tr -d " ")
+CONCOURSE_PASSWORD=$(cat credentials.yml | grep CONCOURSE_PASSWORD | awk 'BEGIN { FS = ":" }; { print $2 }' | tr -d " " | tr -d "\"")
+CONCOURSEURL=$(cat credentials.yml | grep CONCOURSEURL | awk 'BEGIN { FS = ": " }; { print $2 }'| tr -d " " | tr -d "\"")
+fly login -t concourse -c $CONCOURSEURL -k -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWORD
+
+fly -t concourse set-pipeline -p $reponame-$RANDOM -c pipeline.yml -l credentials.yml -n
+fly -t concourse unpause-pipeline -p $reponame-$RANDOM
